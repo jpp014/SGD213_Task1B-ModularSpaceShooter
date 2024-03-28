e2e77552584dd3395b8be87a03919ca5e2884ddd
James Pitor <pitorjames@gmail.com>
2024-03-28 19:46:36 +1000

    removed unnecessary comments

--- Assets/Scripts/PlayerMovementScript.cs
+++ Assets/Scripts/PlayerMovementScript.cs
@@ -2,24 +2,20 @@
 using System.Collections;
 
 public class PlayerMovementScript : MonoBehaviour {
-            
     // HorizontalPlayerAcceleration indicates how fast we accelerate Horizontally
     [SerializeField] private float fHorPlayAccel = 5000f;
     private Rigidbody2D ourRigidbody;
-
-    // Use this for initialization
-    private void Start() {
-    // Get OurRigidbodyComponent once at the start of the game and store a reference to it
-    // This means that we don't need to call GetComponent more than once! This makes our game faster. (GetComponent is SLOW)
+    
+    private void Start() { 
+    // Get RigidbodyComponent reference
     ourRigidbody = GetComponent<Rigidbody2D>(); }
+    
     // Update is called once per frame
     private void Update() {
         var horizontalInput = Input.GetAxis("Horizontal");
-
         if (horizontalInput != 0.0f) { 
             var forceToAdd = Vector2.right*(horizontalInput*fHorPlayAccel*Time.deltaTime);
             ourRigidbody.AddForce(forceToAdd);
-            //print(HorizontalInput);
         }
     } 
 }
