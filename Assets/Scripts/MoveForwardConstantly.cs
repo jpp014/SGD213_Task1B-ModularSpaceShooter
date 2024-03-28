b9e79865b12212e769051dc7d30e560c58112686
James Pitor <pitorjames@gmail.com>
2024-03-28 21:21:39 +1000

    Wrote MoveForwardConstantly.cs as a component of bullet and enemy prefabs

--- Assets/Scripts/MoveForwardConstantly.cs
+++ Assets/Scripts/MoveForwardConstantly.cs
@@ -1,18 +1,40 @@
 ﻿using UnityEngine;
-using System.Collections;
+
 
 public class MoveForwardConstantly : MonoBehaviour
 {
     
+    [SerializeField] float acceleration;
+
+    [SerializeField]  float initialVelocity;
+    
+    private enum Directions
+    {
+        Up = 1,
+        Down = -1
+    };
+
+    [SerializeField] private Directions directions;
+    private Rigidbody2D ourRigidbody;
+
+
     // Use this for initialization
     void Start()
     {
+        ourRigidbody = GetComponent<Rigidbody2D>();
 
+        ourRigidbody.velocity = new Vector2(0f, (float)directions) * initialVelocity;
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        
+        
+        var forceToAdd =new Vector2(0f, (float)directions) * (acceleration * Time.deltaTime);
+        
+        ourRigidbody.AddForce(forceToAdd);
     }
 }
+
+
